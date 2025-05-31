ea7c50db938232cbbec2b8920c61da7954ecd51d
Caleb Froese <caleb.froese@gmail.com>
2025-05-31 14:18:54 +0930

    Update ampcode-cli plugin

--- plugin/ampcode-cli.lua
+++ plugin/ampcode-cli.lua
@@ -8,8 +8,3 @@ vim.api.nvim_create_user_command('Amp', function()
   require('ampcode-cli').run('amp')
 end, {})
 
-vim.api.nvim_create_user_command('AmpcodeClose', function()
-  require('ampcode-cli').close()
-end, {})
-
-
